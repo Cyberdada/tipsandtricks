d895adf86eeb1fbfd74860a86dc9a3e070bd3ef6
Magnus Gudmundsson <MGUDMUN3@volvocars.com>
2018-02-22 15:10:51 +0100

    reock nr rool

--- src/app/solution/solution.component.ts
+++ src/app/solution/solution.component.ts
@@ -15,6 +15,9 @@ export class SolutionComponent implements OnInit, OnDestroy {
   problemSubscription: Subscription;
   problem: any;
 
+  initaDataSubscription: Subscription;
+  intialData = '';
+
   Mode = Mode;
   mode: Mode;
 
@@ -23,18 +26,54 @@ export class SolutionComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.problemSubscription = this.problemStateServive.currentProblem$.subscribe(event =>  {
       this.problem = event;
-      this.problem.solutions.forEach(itm => itm.mode = Mode.Code);
+      this.problem.solutions.forEach(itm =>  {
+        itm.mode = Mode.Code;
+        itm.result = this.beautify(itm.code);
+      });
+    });
+
+    this.initaDataSubscription = this.problemStateServive.initalData$.subscribe(event =>  {
+      this.intialData = event;
     });
   }
 
 
   ngOnDestroy() {
     this.problemSubscription.unsubscribe();
+    this.initaDataSubscription.unsubscribe();
   }
 
 
-  beautify(code: string) {
+
+
+
+  changeResult(solution: any, mode: Mode) {
+
+    solution.code = mode === Mode.Result 
+    ? solution.result
+    : solution.code;
+
+    solution.result = mode === Mode.Code
+      ? this.beautify(solution.code)
+      : this.result(solution.code);
+    }
+
+  private beautify(code: string) {
     return  js_beautify(code);
+  }
+
+  private result(code: string) {
+    let retval = '';
+
+    try {
+       // tslint:disable-next-line:no-eval
+    retval = eval(this.intialData + ' ' + code);
+    retval = JSON.stringify(retval);
+
+    } catch (ex) {
+      retval = ex.message + '\n' + ex.stack;
+  }
+  return retval;
   }
 
 
